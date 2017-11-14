27f7a1fee9214bf9694941f05228c35444bede7d
kacek <kasper.augustyn@gmail.com>
2017-11-14 19:41:11 +0100

    info module stub, ping command

---
+++ ModBot/ModBot/Modules/InfoModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace ModBot
+{
+    public class InfoModule :ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        [Summary("returns response time")]
+        public async Task PingAsync()
+        {
+            await ReplyAsync(":ping_pong: Pong! :stopwatch: " + Context.Client.Latency);
+        }
+    }
+}
