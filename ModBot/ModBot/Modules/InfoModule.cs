1600edde34265bf049ec7a6db612d18d3d56ac5b
kacek <kasper.augustyn@gmail.com>
2017-11-14 21:49:51 +0100

    [INFO]implemented whois and userinfo commands

--- ModBot/ModBot/Modules/InfoModule.cs
+++ ModBot/ModBot/Modules/InfoModule.cs
@@ -3,16 +3,41 @@ using System.Collections.Generic;
 using System.Text;
 using Discord.Commands;
 using System.Threading.Tasks;
+using Discord.WebSocket;
+using Discord;
 
 namespace ModBot
 {
     public class InfoModule :ModuleBase<SocketCommandContext>
     {
         [Command("ping")]
-        [Summary("returns response time")]
+        [Summary("Returns response time.")]
         public async Task PingAsync()
         {
             await ReplyAsync(":ping_pong: Pong! :stopwatch: " + Context.Client.Latency);
+        }
+
+        [Command("whois")]
+        [Summary("Returns global name of specified user.")]
+        public async Task WhoIsAsync([Summary("(optional)user to get global name from.")] SocketUser user = null)
+        {
+            var userInfo = user ?? Context.Message.Author;
+            await ReplyAsync($"{userInfo.Username}#{userInfo.Discriminator}");
+        }
+
+        [Command("userinfo")]
+        [Summary("Returns detailed information about user.")]
+        public async Task UserInfoAsync([Summary("(optional)user to get detailed information about")] SocketUser user = null)
+        {
+            var userInfo = user ?? Context.Message.Author;
+            var embed = new EmbedBuilder();
+            embed.WithAuthor(userInfo);
+            embed.AddField("Local name",((SocketGuildUser)userInfo).Nickname==null ? userInfo.Username : ((SocketGuildUser)userInfo).Nickname);
+            embed.AddField("Status", userInfo.Status.ToString());
+            embed.AddField("In game", userInfo.Game.HasValue ? userInfo.Game.Value.ToString() : "none");
+            embed.AddField("Joined", ((SocketGuildUser)userInfo).JoinedAt.ToString());
+            embed.WithColor(Color.Blue);
+            await Context.Channel.SendMessageAsync("", false, embed);
         }
     }
 }
