78bc47d6382f5151fa8ce9158e16c100383dfddf
kacek <kasper.augustyn@gmail.com>
2017-11-14 17:38:05 +0100

    switch to module based architecture

--- ModBot/ModBot/Program.cs
+++ ModBot/ModBot/Program.cs
@@ -1,29 +1,35 @@
 ﻿using Discord;
 using Discord.WebSocket;
+using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace ModBot
 {
     public class Program
     {
-        public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+        public static void Main(string[] args) => new Program().StartAsync().GetAwaiter().GetResult();
  
         private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
 
-        public async Task MainAsync()
+        public async Task StartAsync()
         {
             _client = new DiscordSocketClient();
+            _commands = new CommandService();
 
             _client.Log += Log;
 
-            _client.MessageReceived += MessageReceived;
-            _client.LatencyUpdated += LatencyUpdated;
-
             Console.WriteLine("Insert Token:");
             string token = Console.ReadLine();
+
+            _services = new ServiceCollection().AddSingleton(_client).AddSingleton(_commands).BuildServiceProvider();
             try
             {
+                await InstallCommandsAsync();
                 await _client.LoginAsync(TokenType.Bot, token);
                 await _client.StartAsync();
             }
@@ -36,18 +42,28 @@ namespace ModBot
             await Task.Delay(-1);
         }
 
+        public async Task InstallCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private async Task HandleCommandAsync(SocketMessage messageParam)
+        {
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+            int argPos = 0;
+            if (!(message.HasCharPrefix('!', ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))) return;
+            var context = new SocketCommandContext(_client, message);
+            var result = await _commands.ExecuteAsync(context, argPos, _services);
+            if (!result.IsSuccess)
+                await context.Channel.SendMessageAsync(result.ErrorReason);
+        }
+
         private Task Log(LogMessage msg)
         {
             Console.WriteLine(msg.ToString());
             return Task.CompletedTask;
-        }
-
-        private async Task MessageReceived(SocketMessage msg)
-        {
-            if (msg.Content == "!ping")
-            {
-                await msg.Channel.SendMessageAsync(":ping_pong: Pong! :stopwatch: "+_client.Latency+"ms");
-            }
         }
     }
 }
